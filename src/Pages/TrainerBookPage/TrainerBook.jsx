031e8152889f4f8ca366521a478b42942b2aa53d
Abdullah's PC <abdullahalrahman23@gmail.com>
2023-11-28 10:18:03 +0300

    added trainer type in book trainer

--- src/Pages/TrainerBookPage/TrainerBook.jsx
+++ src/Pages/TrainerBookPage/TrainerBook.jsx
@@ -1,35 +1,38 @@
-
+import bronz from '../../assets/images/types/bronz.png'
+import silver from '../../assets/images/types/silver.png'
+import gold from '../../assets/images/types/gold.png'
 
 const TrainerBook = () => {
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-4 gap-5">
-            <div className="card w-80 bg-base-100 shadow-xl">
+            <div className="card w-80 bg-orange-200 shadow-xl">
                 <div className="card-body">
-                    <h2 className="card-title">Bronz</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <h2 className="card-title text-center flex justify-center">Bronz</h2>
+                    <img src={bronz} alt="" />
                     <div className="card-actions justify-center">
-                        <button className="btn btn-primary">Buy Now</button>
+                        <button className="btn btn-outline outline-orange-400">Book Now</button>
                     </div>
                 </div>
             </div>
-            <div className="card w-80 bg-base-100 shadow-xl">
+            <div className="card w-80 bg-gray-200 shadow-xl">
                 <div className="card-body">
-                    <h2 className="card-title">Silver</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <h2 className="card-title text-center flex justify-center">Silver</h2>
+                    <img src={silver} alt="" />
                     <div className="card-actions justify-center">
-                        <button className="btn btn-primary">Buy Now</button>
+                        <button className="btn btn-outline outline-orange-400">Book Now</button>
                     </div>
                 </div>
             </div>
-            <div className="card w-80 bg-base-100 shadow-xl">
+            <div className="card w-80 bg-orange-300 shadow-xl">
                 <div className="card-body">
-                    <h2 className="card-title">Gold</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <h2 className="card-title text-center flex justify-center">Gold</h2>
+                    <img src={gold} alt="" />
                     <div className="card-actions justify-center">
-                        <button className="btn btn-primary">Buy Now</button>
+                        <button className="btn btn-outline outline-orange-400">Book Now</button>
                     </div>
                 </div>
             </div>
+
         </div>
     );
 };
