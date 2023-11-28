1d471f3690ce0493c2d4ce05abdbc582971dd72c
Abdullah's PC <abdullahalrahman23@gmail.com>
2023-11-28 10:38:20 +0300

    fixed layout of book trainer

--- src/Pages/TrainerBookPage/TrainerBook.jsx
+++ src/Pages/TrainerBookPage/TrainerBook.jsx
@@ -4,35 +4,68 @@ import gold from '../../assets/images/types/gold.png'
 
 const TrainerBook = () => {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-4 gap-5">
-            <div className="card w-80 bg-orange-200 shadow-xl">
-                <div className="card-body">
-                    <h2 className="card-title text-center flex justify-center">Bronz</h2>
-                    <img src={bronz} alt="" />
-                    <div className="card-actions justify-center">
-                        <button className="btn btn-outline outline-orange-400">Book Now</button>
-                    </div>
-                </div>
-            </div>
-            <div className="card w-80 bg-gray-200 shadow-xl">
-                <div className="card-body">
-                    <h2 className="card-title text-center flex justify-center">Silver</h2>
-                    <img src={silver} alt="" />
-                    <div className="card-actions justify-center">
-                        <button className="btn btn-outline outline-orange-400">Book Now</button>
-                    </div>
-                </div>
-            </div>
-            <div className="card w-80 bg-orange-300 shadow-xl">
-                <div className="card-body">
-                    <h2 className="card-title text-center flex justify-center">Gold</h2>
-                    <img src={gold} alt="" />
-                    <div className="card-actions justify-center">
-                        <button className="btn btn-outline outline-orange-400">Book Now</button>
-                    </div>
-                </div>
-            </div>
+        <div className="grid grid-cols-1 mt-4 gap-5">
+            <div className="hero">
+                <div className="hero-content flex-col lg:flex-row">
+                    <img src={bronz} className="max-w-sm rounded-lg shadow-2xl bg-orange-200" />
+                    <div className=''>
+                        <h2 className='text-primary text-xl'>Bronze Package:</h2>
+                        <ol>
+                            <li>1. Basic Discounts: Members receive standard discounts on purchases.</li>
 
+                            <li>2. Standard Customer Support: Access to standard customer support services.</li>
+
+                            <li>3. Points System: Earn points for each purchase within the loyalty program.</li>
+
+                            <li>4. Limited Access: Access to general promotions and sales.</li>
+
+                            <li>5. Limited Access: Standard Benefits: Basic benefits associated with the loyalty program.</li>
+                        </ol>
+                    </div>
+                    <button className='btn btn-outline'>Join Now</button>
+                </div>
+            </div>
+            <div className="hero">
+                <div className="hero-content flex-col lg:flex-row">
+                    <img src={silver} className="max-w-sm rounded-lg shadow-2xl bg-gray-200" />
+                    <div className=''>
+                        <h2 className='text-primary text-xl'>Silver Package:</h2>
+                        <ol>
+                            <li>1. Higher Discounts: Members enjoy increased discounts on purchases compared to the bronze package.
+                            </li>
+
+                            <li>2. Priority Customer Support: Priority access to customer support with faster response times.</li>
+
+                            <li>3. Enhanced Points System: Earn more points per purchase than the bronze package.</li>
+
+                            <li>4. Exclusive Sales: Access to exclusive sales events and promotions.</li>
+
+                            <li>5. Advanced Benefits: Additional perks such as extended warranty or exclusive member-only content.</li>
+                        </ol>
+                    </div>
+                    <button className='btn btn-outline'>Join Now</button>
+                </div>
+            </div>
+            <div className="hero">
+                <div className="hero-content flex-col lg:flex-row">
+                    <img src={gold} className="max-w-sm rounded-lg shadow-2xl bg-orange-300" />
+                    <div className=''>
+                        <h2 className='text-primary text-xl'>Gold Package:</h2>
+                        <ol>
+                            <li>1. Maximum Discounts: The highest level of discounts on purchases.</li>
+
+                            <li>2. Dedicated Customer Support: Dedicated customer support representatives for personalized assistance.</li>
+
+                            <li>3. Premium Points System: Earn the most points per purchase, accelerating reward accumulation.</li>
+
+                            <li>4. VIP Access: Exclusive access to pre-sales, limited-edition products, and VIP events.</li>
+
+                            <li>5. Elite Benefits: Premium benefits, such as priority shipping, special gifts, and tailored experiences.</li>
+                        </ol>
+                    </div>
+                    <button className='btn btn-outline'>Join Now</button>
+                </div>
+            </div>
         </div>
     );
 };
